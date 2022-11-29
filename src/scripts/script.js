4217f31a36070fb6f5fa6739ceba9ebca9d1e298
Alexander Popov <shurikation@gmail.com>
2022-11-29 16:21:22 +0400

    Add script to toggle search/sort tabs

---
+++ src/scripts/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const searchTabsToggler = () => {
+		const tabs = document.querySelector('.search-tabs');
+		let prevTab = null;
+		tabs.addEventListener('click', (e) => {
+			if (prevTab) prevTab.classList.add('collapse');
+
+			const type = e.target.name;
+			const workspace = document.querySelector(`[data-name=${type}]`);
+			const footer = document.querySelector('.search-footer');
+
+			if (prevTab === workspace) {
+				workspace.classList.add('collapse');
+				footer.classList.add('collapse');
+			} else {
+				workspace.classList.remove('collapse');
+				footer.classList.remove('collapse')
+			}
+			prevTab = workspace;
+		})
+	}
+	searchTabsToggler();
+
+
+});
+
+
